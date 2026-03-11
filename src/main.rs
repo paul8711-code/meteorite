73a6cfaa996d70df01de75612c0486f7bab60f28
Paul8711 <154304377+Paulprojects8711@users.noreply.github.com>
2026-03-11 11:28:48 +0100

    test 1 using matrix sdk 0.14.0 cuz 0.16.0 didnt work (the script is from matrix sdk docs)

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,42 @@
-fn main() {
-    println!("Hello, world!");
+extern crate rpassword;
+
+use matrix_sdk::{
+    Client, config::SyncSettings,
+    ruma::{UserId, events::room::message::SyncRoomMessageEvent},
+};
+
+use std::io;
+use rpassword::read_password;
+
+#[tokio::main]
+async fn main() -> anyhow::Result<()> {
+    println!("input user name");
+
+    let mut user_inp = String::new();
+
+    io::stdin()
+        .read_line(&mut user_inp)
+        .expect("Failed to read line");
+
+    user_inp = user_inp.trim().to_string();
+
+    let user = UserId::parse(&user_inp)?; 
+    let client = Client::builder().server_name(user.server_name()).build().await?;
+
+    println!("input password");
+
+    let mut password_inp = read_password().unwrap();
+
+    // First we need to log in.
+    client.matrix_auth().login_username(user, &password_inp).send().await?;
+
+    client.add_event_handler(|ev: SyncRoomMessageEvent| async move {
+        println!("Received a message {:?}", ev);
+    });
+
+    // Syncing is important to synchronize the client state with the server.
+    // This method will never return unless there is an error.
+    client.sync(SyncSettings::default()).await?;
+
+    Ok(())
 }
